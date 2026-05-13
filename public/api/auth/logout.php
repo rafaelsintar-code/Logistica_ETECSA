e6252ac15b79755230d08b65bbe764a0b6f65aaa
Gumer Perez Garcia <rafaelsintar@gmail.com>
2026-05-13 15:21:30 -0400

    Delete public directory

--- public/api/auth/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode(["success" => true, "redirect" => "../pages/login.html"]);
-?>
